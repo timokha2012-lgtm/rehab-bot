dc2189d4669a6f2fe28e4f1e046f7dbbedec377b
timokha2012-lgtm <timokha2012@gmail.com>
2026-04-13 23:06:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '8359587585:AAH2yIRu4XC3nBW3iHrrQ03g8YBlU-eNcNA';
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN, {
+  polling: true
+});
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Ты в диагностике. Отвечай честно.\n\nВопрос 1:\nТы сейчас живешь в правде или играешь роль?');
+  bot.sendMessage(
+    msg.chat.id,
+    'Ты в диагностике. Отвечай честно.\n\nВопрос 1:\nТы сейчас живешь в правде или играешь роль?'
+  );
 });
 
 bot.on('message', (msg) => {
-    if (msg.text !== '/start') {
-        bot.sendMessage(msg.chat.id, 'Принял. Дальше будет глубже.');
-    }
+  if (msg.text !== '/start') {
+    bot.sendMessage(msg.chat.id, 'Принял. Дальше будет глубже.');
+  }
 });
